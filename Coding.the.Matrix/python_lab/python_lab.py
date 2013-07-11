a0e806839d081306888b599bafd0a3f6b769ac65
murphytalk <murphytalk@gmail.com>
2013-07-12 00:56:47 +0900

    Python Lab done

--- Coding.the.Matrix/python_lab/python_lab.py
+++ Coding.the.Matrix/python_lab/python_lab.py
@@ -68,53 +68,53 @@ L1 = [1,1,2] # <-- want len(L1) != len(list(set(L1)))
 L2 = [3,2,1] # <-- same len(L2) == len(list(set(L2))) but L2 != list(set(L2))
 
 ## Task 18
-odd_num_list_range = {...}
+odd_num_list_range = {i for i in range(100) if i%2!= 0}
 
 ## Task 19
 L = ['A','B','C','D','E']
-range_and_zip = ...
+range_and_zip = list(zip(range(5),L))
 
 ## Task 20
-list_sum_zip = [...]
+list_sum_zip = [i[0]+i[1] for i in zip([10, 25, 40],[1, 15, 20])]
 
 ## Task 21
 dlist = [{'James':'Sean', 'director':'Terence'}, {'James':'Roger', 'director':'Lewis'}, {'James':'Pierce', 'director':'Roger'}]
 k = 'James'
-value_list = [...]
+value_list = [i[k] for i in dlist]
 
 ## Task 22
 dlist = [{'Bilbo':'Ian','Frodo':'Elijah'},{'Bilbo':'Martin','Thorin':'Richard'}]
 k = 'Bilbo'
-value_list_modified_1 = [...] # <-- Use the same expression here
+value_list_modified_1 = [i[k] if k in i else 'NOT PRESENT' for i in dlist] # <-- Use the same expression here
 k = 'Frodo'
-value_list_modified_2 = [...] # <-- as you do here
+value_list_modified_2 = [i[k] if k in i else 'NOT PRESENT' for i in dlist] # <-- as you do here
 
 ## Task 23
-square_dict = {...}
+square_dict = {i:i*i for i in range(100)}
 
 ## Task 24
 D = {'red','white','blue'}
-identity_dict = {...}
+identity_dict = {i:i for i in D}
 
 ## Task 25
 base = 10
 digits = set(range(10))
-representation_dict = {...}
+representation_dict = { x1*base*base+x2*base+x3:[x1,x2,x3] for x1 in digits for x2 in digits for x3 in digits}
 
 ## Task 26
 d = {0:1000.0, 1:1200.50, 2:990}
 names = ['Larry', 'Curly', 'Moe']
-listdict2dict = { ... }
+listdict2dict = { names[k]:v for (k,v) in d.items()}
 
 ## Task 27
-def nextInts(L): return [ ... ]
+def nextInts(L): return [ x+1 for x in L ]
 
 ## Task 28
-def cubes(L): return [ ... ] 
+def cubes(L): return [ x*x*x for x in L ] 
 
 ## Task 29
-def dict2list(dct, keylist): return [ ... ]
+def dict2list(dct, keylist): return [ dct[x] for x in keylist ]
 
 ## Task 30 
-def list2dict(L, keylist): return { ... } 
+def list2dict(L, keylist): return { keylist[i]:L[i] for i in range(len(L)) } 
 
