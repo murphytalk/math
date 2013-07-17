97a3301da11ef1b5b33d151f12d0884ebe4f158a
murphytalk <murphytalk@gmail.com>
2013-07-17 20:08:53 +0900

    hw0

---
+++ Coding.the.Matrix/matrix/hw0.py
@@ -0,0 +1,59 @@
+# Please fill out this stencil and submit using the provided submission script.
+
+
+
+
+
+## Problem 1
+def myFilter(L, num): return [x for x in L if x%num !=0]
+
+
+
+## Problem 2
+def myLists(L): return [list(range(1,x+1)) for x in L]
+
+
+
+## Problem 3
+def myFunctionComposition(f, g): return { k:g[v] for k,v in f.items()}
+
+
+## Problem 4
+# Please only enter your numerical solution.
+
+complex_addition_a = (5+3j)
+complex_addition_b = 1j
+complex_addition_c = (-1+0.001j)
+complex_addition_d = (0.001+9j)
+
+
+
+## Problem 5
+GF2_sum_1 = ...
+GF2_sum_2 = ...
+GF2_sum_3 = ...
+
+
+## Problem 6
+def mySum(L): pass
+
+
+
+## Problem 7
+def myProduct(L): pass
+
+
+
+## Problem 8
+def myMin(L): pass
+
+
+
+## Problem 9
+def myConcat(L): pass
+
+
+
+## Problem 10
+def myUnion(L): pass
+
