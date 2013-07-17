50beccf50f4030d50bf0701ee68136ffcd51cd60
murphytalk <murphytalk@gmail.com>
2013-07-17 21:23:37 +0900

    hw0 done

--- Coding.the.Matrix/matrix/hw0.py
+++ Coding.the.Matrix/matrix/hw0.py
@@ -29,31 +29,52 @@ complex_addition_d = (0.001+9j)
 
 
 ## Problem 5
-GF2_sum_1 = ...
-GF2_sum_2 = ...
-GF2_sum_3 = ...
+GF2_sum_1 = 1
+GF2_sum_2 = 0
+GF2_sum_3 = 0
 
 
 ## Problem 6
-def mySum(L): pass
-
+def mySum(L):
+    current = 0
+    for x in L:
+        current = current + x
+    return current
 
 
 ## Problem 7
-def myProduct(L): pass
-
-
+def myProduct(L):
+    current = 1
+    for x in L:
+        current = current * x
+    return current
+    
 
 ## Problem 8
-def myMin(L): pass
+def myMin(L):
+    current = None
+    for x in L:
+        if current is None:
+            current = x
+        elif x < current:
+            current = x
+    return current
 
 
 
 ## Problem 9
-def myConcat(L): pass
-
+def myConcat(L):
+    current = ''
+    for x in L:
+        current = current + x
+    return current
 
 
 ## Problem 10
-def myUnion(L): pass
+def myUnion(L):
+    current = set()
+    for x in L:
+        current = current | x
+    return current
+
 
